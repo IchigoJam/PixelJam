95ab50bd06b3334d3c1c680c858c6a4e9fafe0cf
Taisuke Fukuno <fukuno@jig.jp>
2026-01-17 17:35:44 +0900

    separate module

---
+++ PANCAKE_COLORS.js
@@ -0,0 +1,41 @@
+// CC BY NATURAL STYLE - http://pancake.shizentai.jp/
+
+/*
+import { BLACK, WHITE, RED, PINK, ORANGE, YELLOW, L_BROWN, BROWN, L_GREEN, GREEN, L_BLUE, BLUE, D_BLUE, B_PURPLE, R_PURPLE, GRAY } from "./PANCAKE_COLORS.js";
+*/
+
+export const PANCAKE_COLORS = [
+  [0, 0, 0],
+  [255, 255, 255],
+  [227, 27, 41],
+  [255, 104, 139],
+  [242, 126, 48],
+  [255, 203, 61],
+  [255, 222, 169],
+  [107, 74, 43],
+  [145, 202, 24],
+  [28, 75, 55],
+  [67, 175, 215],
+  [38, 74, 208],
+  [26, 36, 102],
+  [99, 55, 187],
+  [178, 63, 171],
+  [204, 204, 204]
+];
+
+export const BLACK = 0;
+export const WHITE = 1;
+export const RED = 2;
+export const PINK = 3;
+export const ORANGE = 4;
+export const YELLOW = 5;
+export const L_BROWN = 6;
+export const BROWN = 7;
+export const L_GREEN = 8;
+export const GREEN = 9;
+export const L_BLUE = 10;
+export const BLUE = 11;
+export const D_BLUE = 12;
+export const B_PURPLE = 13;
+export const R_PURPLE = 14;
+export const GRAY = 15;
